7757ffb567cc670a82ce6923d834eab337c713da
wuzhuang5 <wuz0316@gmail.com>
2019-05-15 23:06:53 +0800

    init

---
+++ summer-mall-admin/src/main/java/com/wd/config/Swagger2Config.java
@@ -0,0 +1,8 @@
+package com.wd.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+public class Swagger2Config {
+}
--- summer-mall-admin/src/test/java/com/wd/controller/SummerMallAdminApplicationTests.java
+++ summer-mall-admin/src/test/java/com/wd/controller/SummerMallAdminApplicationTests.java
@@ -1,4 +1,4 @@
-package com.wd.summermalladmin;
+package com.wd.controller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
